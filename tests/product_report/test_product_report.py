3f9987025840a6d38bbfa63beb2241b8ecbbc8f9
biel-igl <gabrielpires524@gmail.com>
2023-08-25 15:37:43 -0300

    feat: faz testes para req 02

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,21 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    produto = Product(
+        "001",
+        "paçoquinha",
+        "Brasil",
+        "26/06/2023",
+        "26/08/2023",
+        "123456789",
+        "Frágio",
+    )
+
+    assert (
+        f"{produto}"
+        == "The product 001"
+        " - paçoquinha with serial number 123456789 manufactured on"
+        " 26/06/2023 by the company Brasil valid until 26/08/2023 "
+        "must be stored according to the following instructions: Frágio."
+    )
