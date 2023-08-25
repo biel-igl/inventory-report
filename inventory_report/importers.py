330975c5b594944f4183bb7c3c68bb809d6b4bc8
biel-igl <gabrielpires524@gmail.com>
2023-08-25 16:18:36 -0300

    feat: faz req 04

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import json
 from typing import Dict, List, Type
 
 from inventory_report.product import Product
@@ -6,15 +7,31 @@ from inventory_report.product import Product
 
 class Importer(ABC):
     def __init__(self, path: str) -> None:
-        self.path == path
+        self.path = path
 
     @abstractmethod
     def import_data(self) -> List[Product]:
         pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> list[Product]:
+        produtos = []
+        with open(self.path) as file:
+            products = json.load(file)
+            produtos = [
+                Product(
+                    product["id"],
+                    product["product_name"],
+                    product["company_name"],
+                    product["manufacturing_date"],
+                    product["expiration_date"],
+                    product["serial_number"],
+                    product["storage_instructions"],
+                )
+                for product in products
+            ]
+        return produtos
 
 
 class CsvImporter:
