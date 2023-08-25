2b98750405b95ad38cb3ec2a8d76f15fe4ec09f3
biel-igl <gabrielpires524@gmail.com>
2023-08-25 15:31:45 -0300

    feat: inicia o projeto com o req 01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,20 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    produto = Product(
+        "001",
+        "paçoquinha",
+        "Brasil",
+        "26/06/2023",
+        "26/08/2023",
+        "123456789",
+        "Frágio",
+    )
+    assert produto.company_name == "Brasil"
+    assert produto.expiration_date == "26/08/2023"
+    assert produto.id == "001"
+    assert produto.manufacturing_date == "26/06/2023"
+    assert produto.product_name == "paçoquinha"
+    assert produto.serial_number == "123456789"
+    assert produto.storage_instructions == "Frágio"
